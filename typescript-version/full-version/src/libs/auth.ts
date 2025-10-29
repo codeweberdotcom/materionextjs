c0c146bdca53d68a87bf5c5af834d73e13aff89c
Denis Korablev <info@codeweber.com>
2025-10-29 16:48:48 +0300

    bccryptjs

--- typescript-version/full-version/src/libs/auth.ts
+++ typescript-version/full-version/src/libs/auth.ts
@@ -3,7 +3,7 @@ import CredentialProvider from 'next-auth/providers/credentials'
 import GoogleProvider from 'next-auth/providers/google'
 import type { NextAuthOptions, User } from 'next-auth'
 import type { Adapter } from 'next-auth/adapters'
-import bcrypt from 'bcrypt'
+import bcrypt from 'bcryptjs'
 
 // Initialize Prisma client
 import { PrismaClient } from '@prisma/client'
